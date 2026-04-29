128533312de814b85280148e1942a4b2041bef0c
Acik_Web <firdausahlul66@gmail.com>
2026-04-30 06:11:31 +0800

    Update weather_app from Acode

--- bin/weather_app.dart
+++ bin/weather_app.dart
@@ -1,5 +1,23 @@
-import 'package:weather_app/weather_app.dart' as weather_app;
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) {
-  print('Hello world: ${weather_app.calculate()}!');
+const apiKey = 'c3606858261aaad270807afed99959d3';
+
+Future<void> main() async {
+  stdout.write('Masukkan nama kota: ');
+  String city = stdin.readLineSync()!;
+
+  final url = Uri.parse(
+    'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric&lang=id'
+  );
+
+  final response = await http.get(url);
+  final data = jsonDecode(response.body);
+
+  print('\n📍 Kota: ${data['name']}');
+  print('🌤️  Cuaca: ${data['weather'][0]['description']}');
+  print('🌡️  Suhu: ${data['main']['temp']}°C');
+  print('💧 Kelembaban: ${data['main']['humidity']}%');
+  print('💨 Angin: ${data['wind']['speed']} m/s');
 }
